8539d36bf04275f6ef83d514a24dfe9e530c1731
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-05-14 03:16:45 +0530

    Deferring the App Install Banner

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
@@ -5,3 +7,10 @@ if ('serviceWorker' in navigator) {
       console.log('Service worker registered!');
     });
 }
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
